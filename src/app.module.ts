e411bfe6c634604cfbb2444ab421715f0f4afc79
cliffordten <cliffordteng5@gmail.com>
2022-06-20 00:39:09 +0100

    added graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
-import { HelloModule } from './modules/hello/hello.module';
-
+import { HelloModule } from './graphql/hello/hello.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UserModule } from './graphql/user/user.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 @Module({
-  imports: [HelloModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: Boolean(process.env.DEBUG),
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    HelloModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   SwaggerModule.setup('/', app, document);
 
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
